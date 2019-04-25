e135d2538ffc1b1d1d3c92d38ac8079333f76226
chenyi <1260573685@qq.com>
2019-04-25 19:48:08 +0800

    增加模糊查询，多条件查询

--- src/com/cy/t/TestMybatis.java
+++ src/com/cy/t/TestMybatis.java
@@ -2,7 +2,9 @@ package com.cy.t;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -23,17 +25,28 @@ public class TestMybatis {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         SqlSession session = sqlSessionFactory.openSession();
 
-        System.out.println("=================增加====================");
-        category cas=new category();
-        cas.setName("测试3");
-        //提交
-        session.insert("insertCategory",cas);
-        listAll(session);
+//        System.out.println("=================增加====================");
+//        category cas=new category();
+//        cas.setName("测试3");
+//        //提交
+//        session.insert("insertCategory",cas);
+//        listAll(session);
+//
+//        System.out.println("=================删除====================");
+//        cas.setId(2);
+//        session.delete("deleteCategory",cas);
+//        listAll(session);
+//
+//        System.out.println("=================修改====================");
+//        category us=session.selectOne("getCategory",9);
+//        us.setName("测3的爸爸！");
+//        session.update("updateCategory",us);
+//        listAll(session);
+        listAllt(session);
 
-        System.out.println("=================删除====================");
-        cas.setId(2);
-        session.delete("deleteCategory",cas);
-        listAll(session);
+
+
+
 
 
 
@@ -50,6 +63,17 @@ public class TestMybatis {
         List<category> cs = session.selectList("listCategory");
         for (category c : cs) {
             System.out.println(c.getName());
+        }
+
+    }
+    private static void listAllt(SqlSession session)
+    {
+        Map<String,Object> np =new HashMap();
+        np.put("id",5);
+        np.put("name","爸爸");
+        List<category> cs = session.selectList("listByIdName",np);
+        for (category c : cs) {
+            System.out.println(c.getId()+","+c.getName());
         }
 
     }
